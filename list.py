44550344dd582d5a50d449549d794a4b896e197b
sultee <sultlus@gmail.com>
2019-12-02 19:38:28 +0200

    Create list.py

---
+++ list.py
@@ -0,0 +1,7 @@
+* RICOH THETA V 360
+* DJI Osmo mobile 3 Combo
+* Rode Smartlav+ Lavalier Microphone for Smartphone
+* How To: Absurd Scientific Advice for Common Real-World Problems
+* Adafruit Circuit Playground Express
+* Adafruit TensorFlow Lite for Microcontrollers Kit
+* R601 JW3 TTI COMBAT MASTER
