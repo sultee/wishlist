a3b45524220765531369d5f864928a2be7766b5b
sultee <sultlus@gmail.com>
2019-12-16 20:03:08 +0200

    Update list.py

--- list.py
+++ list.py
@@ -1,9 +1,20 @@
+Stuff
 * RICOH THETA V 360
 * DJI Osmo mobile 3 Combo
 * Rode Smartlav+ Lavalier Microphone for Smartphone
-* How To: Absurd Scientific Advice for Common Real-World Problems
 * Adafruit Circuit Playground Express
 * Adafruit TensorFlow Lite for Microcontrollers Kit
 * R601 JW3 TTI COMBAT MASTER
 * Sandmarc Anamorphic Lens Edition - iPhone 11 Pro Max
+
+Books
 * Permanent Record by Edward Snowden
+* How To: Absurd Scientific Advice for Common Real-World Problems by Randall Munroe
+* Bad Blood: Secrets and Lies in a Silicon Valley Startup by John Carreyrou
+* Shoe Dog by Phil Knight
+* Seveneves by Neal Stephenson
+* Sapiens: A Brief History of Humankind by Yuval Noah Harari
+* Thing Explainer: Complicated Stuff in Simple Words by Randall Munroe
+* The Box: How the Shipping Container Made the World Smaller and the World Economy Bigger, by Marc Levinson
+* Where Good Ideas Come From: The Natural History of Innovation by Steven Johnson
+* Digitalis: How to reinvent the world by Thierry Geerts
