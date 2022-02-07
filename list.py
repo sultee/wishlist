a683187b9fdf960b0d50218129fc6357a523e95d
sultee <sultlus@gmail.com>
2022-02-07 13:25:52 +0200

    Update list.py

--- list.py
+++ list.py
@@ -2,7 +2,6 @@ Books
 * Several Short Sentences about Writing by Verlyn Klinkenborg
 * How To: Absurd Scientific Advice for Common Real-World Problems by Randall Munroe
 * Bad Blood: Secrets and Lies in a Silicon Valley Startup by John Carreyrou
-* Shoe Dog by Phil Knight
 * Seveneves by Neal Stephenson
 * Thing Explainer: Complicated Stuff in Simple Words by Randall Munroe
 * The Box: How the Shipping Container Made the World Smaller and the World Economy Bigger, by Marc Levinson
